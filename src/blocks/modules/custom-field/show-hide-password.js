f5425814ddfdc2d3b167e425cde0892201b292aa
Barto <illya1004work@gmail.com>
2021-06-03 22:55:51 +0300

    add toggle visibility password function

---
+++ src/blocks/modules/custom-field/show-hide-password.js
@@ -0,0 +1,22 @@
+const togglePasswordButtons = document.querySelectorAll('.custom-field__show-password');
+const showPasswordAriaText = 'Показати пароль? Увага Ваш пароль буде показано на екрані';
+const hidePasswordAriaText = 'Сховати пароль';
+const showPasswordIconPath = '/img/sprites/sprite.svg#visibility-icon';
+const hidePasswordIconPath = '/img/sprites/sprite.svg#visibility-off-icon';
+
+function togglePasswordVisibility() {
+  const parent = this.closest('.custom-field');
+  const passwordInput = parent.querySelector('.custom-field__input');
+  const icon = this.querySelector('use');
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    this.setAttribute('aria-label', hidePasswordAriaText);
+    icon.setAttribute('xlink:href', showPasswordIconPath);
+  } else {
+    passwordInput.type = 'password';
+    this.setAttribute('aria-label', showPasswordAriaText);
+    icon.setAttribute('xlink:href', hidePasswordIconPath);
+  }
+}
+
+togglePasswordButtons.forEach((button) => button.addEventListener('click', togglePasswordVisibility));
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,6 @@
-// Header
+/* eslint-disable import/no-unresolved,import/extensions */
+import 'focus-visible';
+
 import '%modules%/header/header';
 import '%modules%/custom-modal/custom-modal';
+import '%modules%/custom-field/show-hide-password';
