ecbd9c703f37047aa3cf9009b2391481fba983c5
Barto <illya1004work@gmail.com>
2021-06-05 22:58:55 +0300

    validation

--- src/blocks/modules/custom-field/custom-field.js
+++ src/blocks/modules/custom-field/custom-field.js
@@ -1,11 +1,13 @@
+import FormValidator from '../../../js/controllers/form-validator/FormValidator';
+
 const form = document.querySelector('form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  const values = {};
-  data.forEach((value, key) => {
-    values[key] = value;
-  });
-  console.log(values);
-});
+const rules = {
+  name: 'required',
+  email: ['required', 'email'],
+  password: ['required', { min_length: 10 }],
+  'current-password': ['required', { length_between: [2, 10] }],
+};
+
+const loginValidation = new FormValidator(form);
+loginValidation.init(rules);
--- src/js/controllers/form-validator/FormValidator.js
+++ src/js/controllers/form-validator/FormValidator.js
@@ -1,1 +1,101 @@
+/* eslint-disable no-underscore-dangle,lines-between-class-members */
+import LIVR from 'livr';
 
+export default class FormValidator {
+  #errors;
+  #form;
+  #validationInputsArr;
+
+  constructor(form, autoTrim = true) {
+    this.#form = form;
+    this._isAutoTrim = autoTrim;
+    this.#errors = {};
+    this.#validationInputsArr = [];
+    this.#autoTrimValues();
+  }
+
+  init = (rules) => {
+    this._validator = new LIVR.Validator(rules);
+    this.#form.addEventListener('submit', this.#validationInputs);
+    this.#validationInputsArr = Object.keys(rules);
+    this.#validationInputsArr.forEach((input) => {
+      this.#form.querySelector(`[name="${input}"]`)
+        .addEventListener('blur', this.#checkErrors);
+    });
+  };
+
+  destroy = () => {
+    this._validator = null;
+    this.#form.removeEventListener('submit', this.#validationInputs);
+  };
+
+  get formData() {
+    const data = new FormData(this.#form);
+    const values = {};
+    data.forEach((value, key) => {
+      values[key] = value;
+    });
+    return values;
+  }
+
+  #validationInputs = (evt) => {
+    evt.preventDefault();
+    const validData = this.#checkErrors();
+    if (validData) {
+      this.#form.submit();
+    } else {
+      this.#vibrate();
+    }
+  };
+
+  get errors() {
+    return this.#errors;
+  }
+
+  #autoTrimValues = () => {
+    if (this._isAutoTrim) {
+      LIVR.Validator.defaultAutoTrim(true);
+    }
+  };
+
+  #showInputErrors = () => {
+    const errorsKey = Object.keys(this.#errors);
+    const errorFields = document.querySelectorAll('.custom-field--error');
+    errorFields.forEach((input) => input.classList.remove('custom-field--error'));
+    if (errorsKey.length) {
+      errorsKey.forEach((inputName) => {
+        const input = this.#form.querySelector(`[name="${inputName}"]`);
+        const field = input.closest('.custom-field');
+        field.classList.add('custom-field--error');
+      });
+    }
+  };
+
+  #showInputSuccess = () => {
+    const inputs = this.#form.querySelectorAll('input:not([type="hidden"])');
+    inputs.forEach((input) => {
+      const parent = input.closest('.custom-field');
+      if (!parent.classList.contains('custom-field--error')) {
+        parent.classList.add('custom-field--success');
+      }
+    });
+  };
+
+  #checkErrors = () => {
+    const { formData } = this;
+    const validData = this._validator.validate(formData);
+    if (validData) {
+      return true;
+    }
+    this.#errors = this._validator.getErrors();
+    this.#showInputErrors();
+    this.#showInputSuccess();
+    return false;
+  };
+
+  #vibrate = () => {
+    if (window.navigator.vibrate) {
+      window.navigator.vibrate([300, 100, 300]);
+    }
+  };
+}
